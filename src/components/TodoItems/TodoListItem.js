433123132e434e2726770ebf1d04ebfd30285eab
Himanshu Bisht <himanshubisht313@gmail.com>
2022-06-16 23:46:41 +0530

    added css modules, todo item date format

--- src/components/TodoItems/TodoListItem.js
+++ src/components/TodoItems/TodoListItem.js
@@ -1,47 +1,5 @@
 import icons from '../../static/icons/icons.svg';
-import './TodoListItem.scss';
-
-import styled from 'styled-components';
-
-const ItemName = styled.p`
-  font-size: 1rem;
-  width: 13rem;
-  text-overflow: ellipsis;
-  overflow: hidden;
-  white-space: nowrap;
-
-  @media only screen and (max-width: 30em) {
-    font-size: 0.8rem;
-    width: 8rem;
-  }
-`;
-
-const ItemDate = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 10rem;
-
-  & p {
-    font-size: 1rem;
-    margin: 0 0.2rem;
-
-    @media only screen and (max-width: 30em) {
-      font-size: 0.8rem;
-    }
-  }
-
-  & svg {
-    height: 1.3rem;
-    width: 1.3rem;
-    fill: var(--color-1);
-
-    @media only screen and (max-width: 30em) {
-      height: 1.2rem;
-      width: 1.2rem;
-    }
-  }
-`;
+import styles from './TodoListItem.module.scss';
 
 function TodoListItem(props) {
   const endDate = generateDate(props.endDate);
@@ -53,38 +11,36 @@ function TodoListItem(props) {
   };
 
   return (
-    <li className="todo__list--item" data-id={props.id}>
-      {/* <p className="item__name">{props.name}</p> */}
-      <ItemName>{props.name}</ItemName>
+    <li className={styles['todo__list--item']} data-id={props.id}>
+      <p className={styles['item__name']} title={props.name}>
+        {props.name}
+      </p>
 
-      {/* <div className="item__date">
+      <div className={styles['item__date']}>
         <svg>
           <use href={`${icons}#icon-calendar`}></use>
         </svg>
         <p>{endDate}</p>
-      </div> */}
+      </div>
 
-      <ItemDate>
-        <svg>
-          <use href={`${icons}#icon-calendar`}></use>
-        </svg>
-        <p>{endDate}</p>
-      </ItemDate>
-
-      <div className="item__action">
-        <button title="info" className="item__action--info">
+      <div className={styles['item__action']}>
+        <button title="info" className={styles['item__action--info']}>
           <svg>
             <use href={`${icons}#icon-info`}></use>
           </svg>
         </button>
-        <div className="info__tooltip">
-          <div className="info__tooltip--category">{props.category}</div>
-          <div className="info__tooltip--date">Created: {itemCreateDate}</div>
-          <div className="info__tooltip--text">{props.text}</div>
+        <div className={styles['info__tooltip']}>
+          <div className={styles['info__tooltip--category']}>
+            {props.category}
+          </div>
+          <div className={styles['info__tooltip--date']}>
+            Created: {itemCreateDate}
+          </div>
+          <div className={styles['info__tooltip--text']}>{props.text}</div>
         </div>
         <button
           title="delete"
-          className="item__action--delete"
+          className={styles['item__action--delete']}
           onClick={deleteHandler}
         >
           <svg>
@@ -97,9 +53,12 @@ function TodoListItem(props) {
 }
 
 function generateDate(date) {
-  return `${date.getDate()}th ${date.toLocaleString('en-us', {
-    month: 'long',
-  })}, ${date.getFullYear()}`;
+  return `${String(date.getDate()).padStart(2, 0)}th ${date.toLocaleString(
+    'en-us',
+    {
+      month: 'long',
+    }
+  )}, ${date.getFullYear()}`;
 }
 
 export default TodoListItem;
