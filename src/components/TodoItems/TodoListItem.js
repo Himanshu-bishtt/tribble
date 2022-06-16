3ccc7dde7e41b111124e3047c0fdeec0c634a68e
Himanshu Bisht <himanshubisht313@gmail.com>
2022-06-16 19:28:32 +0530

    added media queries for mobile

--- src/components/TodoItems/TodoListItem.js
+++ src/components/TodoItems/TodoListItem.js
@@ -1,6 +1,41 @@
 import icons from '../../static/icons/icons.svg';
 import './TodoListItem.scss';
 
+import styled from 'styled-components';
+
+const ItemName = styled.p`
+  font-size: 1rem;
+
+  @media only screen and (max-width: 30em) {
+    font-size: 0.8rem;
+  }
+`;
+
+const ItemDate = styled.div`
+  display: flex;
+  align-items: center;
+
+  & p {
+    font-size: 1rem;
+    margin: 0 0.2rem;
+
+    @media only screen and (max-width: 30em) {
+      font-size: 0.8rem;
+    }
+  }
+
+  & svg {
+    height: 1.3rem;
+    width: 1.3rem;
+    fill: var(--color-1);
+
+    @media only screen and (max-width: 30em) {
+      height: 1.2rem;
+      width: 1.2rem;
+    }
+  }
+`;
+
 function TodoListItem(props) {
   const endDate = generateDate(props.endDate);
 
@@ -12,14 +47,22 @@ function TodoListItem(props) {
 
   return (
     <li className="todo__list--item" data-id={props.id}>
-      <p className="item__name">{props.name}</p>
+      {/* <p className="item__name">{props.name}</p> */}
+      <ItemName>{props.name}</ItemName>
 
-      <div className="item__date">
+      {/* <div className="item__date">
         <svg>
           <use href={`${icons}#icon-calendar`}></use>
         </svg>
         <p>{endDate}</p>
-      </div>
+      </div> */}
+
+      <ItemDate>
+        <svg>
+          <use href={`${icons}#icon-calendar`}></use>
+        </svg>
+        <p>{endDate}</p>
+      </ItemDate>
 
       <div className="item__action">
         <button title="info" className="item__action--info">
