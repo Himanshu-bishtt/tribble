e424e828d3f390085880f96e29e2bd96908e68b0
Himanshu Bisht <himanshubisht313@gmail.com>
2022-06-16 19:36:32 +0530

    minor media queries added

--- src/components/TodoItems/TodoListItem.js
+++ src/components/TodoItems/TodoListItem.js
@@ -5,15 +5,22 @@ import styled from 'styled-components';
 
 const ItemName = styled.p`
   font-size: 1rem;
+  width: 13rem;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
 
   @media only screen and (max-width: 30em) {
     font-size: 0.8rem;
+    width: 8rem;
   }
 `;
 
 const ItemDate = styled.div`
   display: flex;
   align-items: center;
+  justify-content: center;
+  width: 10rem;
 
   & p {
     font-size: 1rem;
