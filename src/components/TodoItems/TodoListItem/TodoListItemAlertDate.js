782011ae56fc10a0bbc82810a4c7b2b89162585c
Himanshu Bisht <himanshubisht313@gmail.com>
2022-06-25 18:19:21 +0530

    added different color for expired items

--- src/components/TodoItems/TodoListItem/TodoListItemAlertDate.js
+++ src/components/TodoItems/TodoListItem/TodoListItemAlertDate.js
@@ -4,16 +4,24 @@ import styles from './TodoListItemAlertDate.module.scss';
 
 const TodoListItemAlertDate = ({ daysLeft }) => {
   let dateText;
+  let expired = false;
 
   if (daysLeft < 0) {
     dateText = 'Expired';
+    expired = true;
   } else if (daysLeft === 0) {
     dateText = 'Expires today';
   } else {
     dateText = `${daysLeft} ${daysLeft > 1 ? 'days' : 'day'} left`;
   }
 
-  return <div className={styles['item__date']}>{dateText}</div>;
+  return (
+    <div
+      className={`${styles['item__date']} ${expired ? styles['expired'] : ''}`}
+    >
+      {dateText}
+    </div>
+  );
 };
 
 export default TodoListItemAlertDate;
