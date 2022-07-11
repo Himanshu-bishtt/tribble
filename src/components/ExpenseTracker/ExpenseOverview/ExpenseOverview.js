83ea7f9b023f562a76949477691f2ffafde0ff41
Himanshu Bisht <himanshubisht313@gmail.com>
2022-07-11 22:22:21 +0530

    completed expense overview component

--- src/components/ExpenseTracker/ExpenseOverview/ExpenseOverview.js
+++ src/components/ExpenseTracker/ExpenseOverview/ExpenseOverview.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import Card from '../../UI/Card';
 
+import icones from '../../../static/icons/icons.svg';
 import styles from './ExpenseOverview.module.scss';
 
 function ExpenseOverview() {
   return (
     <div className={styles['expense__overview']}>
-      <Card>
-        <h1>Total Balance</h1>
-        <h3>$25,000</h3>
+      <Card className={styles['expense__overview--card']}>
+        <h1 className={styles['expense__overview--name']}>Total Balance</h1>
+        <h3 className={styles['expense__overview--amount']}>$25,000</h3>
       </Card>
-      <Card>
-        <h1>Total Balance</h1>
-        <h3>$25,000</h3>
+      <Card className={styles['expense__overview--card']}>
+        <h1 className={styles['expense__overview--name']}>Total Income</h1>
+        <h3
+          className={`${styles['expense__overview--amount']} ${styles['expense__overview--amount-income']}`}
+        >
+          +$25,000
+        </h3>
+        <svg
+          className={`${styles['expense__overview--icon']} ${styles['expense__overview--icon-income']}`}
+        >
+          <use href={`${icones}#icon-arrow-down`}></use>
+        </svg>
       </Card>
-      <Card>
-        <h1>Total Balance</h1>
-        <h3>$25,000</h3>
+      <Card className={styles['expense__overview--card']}>
+        <h1 className={styles['expense__overview--name']}>Total Expense</h1>
+        <h3
+          className={`${styles['expense__overview--amount']} ${styles['expense__overview--amount-expense']}`}
+        >
+          -$25,000
+        </h3>
+        <svg
+          className={`${styles['expense__overview--icon']} ${styles['expense__overview--icon-expense']}`}
+        >
+          <use href={`${icones}#icon-arrow-up`}></use>
+        </svg>
       </Card>
     </div>
   );
