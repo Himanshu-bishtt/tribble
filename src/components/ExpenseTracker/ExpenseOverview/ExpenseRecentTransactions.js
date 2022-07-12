173fffebd08aa729f0c3768e6ba79acfb5630ea0
Himanshu Bisht <himanshubisht313@gmail.com>
2022-07-12 23:39:21 +0530

    showing only 4 recent transaction and not more

--- src/components/ExpenseTracker/ExpenseOverview/ExpenseRecentTransactions.js
+++ src/components/ExpenseTracker/ExpenseOverview/ExpenseRecentTransactions.js
@@ -11,17 +11,20 @@ const ExpenseRecentTransactions = props => {
         Recent Transactions
       </div>
       <div className={styles['expense__transactions']}>
-        {props.transactions.map(transaction => (
-          <ExpenseTransactionItem
-            key={transaction.id}
-            name={transaction.name}
-            category={transaction.category}
-            type={transaction.type}
-            date={transaction.date}
-            time={transaction.time}
-            amount={transaction.amount}
-          />
-        ))}
+        {props.transactions.map((transaction, index) => {
+          if (index >= 4) return;
+          return (
+            <ExpenseTransactionItem
+              key={transaction.id}
+              name={transaction.name}
+              category={transaction.category}
+              type={transaction.type}
+              date={transaction.date}
+              time={transaction.time}
+              amount={transaction.amount}
+            />
+          );
+        })}
       </div>
     </React.Fragment>
   );
